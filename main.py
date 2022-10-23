98ad2080bbd9fc7fc4d9242fc5b4fb9b1695bead
Artem_Vaschenko <haaldhd@gmail.com>
2022-10-23 13:17:03 +0300

    commit befour push

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-
+@bot.message_handler(commands=['start'])
+def welkome(message):
+    sti = open('/Users/apple/Desktop/astma.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+    bot.send_message(message.chat.id, 'Добро пожаловать мой маленький Уайтбой'.format(message.from_user, bot. get_me()))
+    parse_mode='html'
 @bot.message_handler(content_types=['text'])
 
 def lalala(message):
